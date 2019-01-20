6d45c6e2c4bb6b6c35c23755471050a39666a7b1
Tom Dane <tjdane@gmail.com>
2019-01-19 21:21:37 -0800

    Add science page

---
+++ src/pages/science.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div style={{ color: `green` }}>
+    <h1>Science</h1>
+    <p>Live Ocean Conditions</p>
+    <p>Tsnuami Detection</p>
+    <p>Whale and fish monitoring</p>
+  </div>
+);
