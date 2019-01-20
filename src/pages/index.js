4b8f5d21a2a74c826ac6600d1e4c4e0e0c53fb22
Tom Dane <tjdane@gmail.com>
2019-01-19 20:51:23 -0800

    Init commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+    <div style={{ color: `blue`}}>
+        <h1>OCIUS</h1>
+        <p>Satellites of the Sea</p>
+        <img src="https://www.business.gov.au/-/media/Business/RDTI/Images/Occius.jpg" alt="Ocius ocean drone: a blue boat sailing on the ocean at Sydney harbour"/>
+    </div>
+);
