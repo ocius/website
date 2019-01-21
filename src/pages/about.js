600c6ab757f87aedab5831b5a551a7e106de64fe
Tom Dane <tjdane@gmail.com>
2019-01-21 01:45:39 -0800

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../components/nav";
+import Header from "../components/header";
+
+export default () => (
+  <div>
+    <Nav />
+    <Header content="About" />
+    <p>About Ocius</p>
+  </div>
+);
