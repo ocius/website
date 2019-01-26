29fb1f77fb0c3283c125d560c260e38e1b8d1aea
Tom Dane <tjdane@gmail.com>
2019-01-26 03:24:35 -0800

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "../components/nav";
+import Header from "../components/header";
+
+export default () => (
+  <div>
+    <Nav />
+    <Header content="Contact" />
+  </div>
+);
