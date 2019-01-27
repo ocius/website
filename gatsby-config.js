1009c2dc3e73362d61e7905df5c5065800885e05
Tom Dane <tjdane@gmail.com>
2019-01-26 20:22:40 -0800

    Add title tag with helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,24 @@ module.exports = {
   siteMetadata: {
     title: `Ocius Technology`,
     siteUrl: `https://ocius.com.au`,
-    description: `We make ocean drones`,
+    description: `We make ocean drones`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/ocius-logo.png" // This path is relative to the root of the site.
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Helmet } from "react-helmet";
 
 export default () => (
   <Layout>
+    <Helmet>
+      <title>My App's Second Title</title>
+    </Helmet>
     <p>Satellites of the Sea</p>
     <img
       src="https://ocius.com.au/wp-content/uploads/2017/09/wallpaperocius-768x432.jpg"
