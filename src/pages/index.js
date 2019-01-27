9393e2b08ad9ec7453d87d73ad0aa682109457ff
Tom Dane <tjdane@gmail.com>
2019-01-26 18:06:18 -0800

    Start adding layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,10 @@ import Title from "../components/title";
 import Helmet from "react-helmet";
 
 export default () => (
-    <div>
-        <Helmet title="Ocius Technology"/>
+    <Layout>
         <Nav />
         <Title />
         <p>Satellites of the Sea</p>
         <img src="https://ocius.com.au/wp-content/uploads/2017/09/wallpaperocius-768x432.jpg" alt="Ocius ocean drone: a blue boat sailing on the ocean at Sydney harbour"/>
-    </div>
+    </Layout>
 );
