db41d9f87666f64bdca07d974aa15f7cf32d9a84
Sergey Monin <moninsergei@gmail.com>
2019-02-07 01:17:41 +0400

    Copy navbar style from main site, refactor NavbarItems component

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -4,48 +4,10 @@ import Radium from 'radium';
 
 const styles = {
   base: {
-    margin: '7.5px -15px',
+    margin: '0',
+    textAlign: 'right',
     listStyle: 'outside none none',
-    paddingLeft: '0',
-    boxSizing: 'border-box',
-
-    fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
-    fontSize: '14px',
-
-    '@media (min-width: 768px)': {
-      float: 'left',
-      margin: '0'
-    }
-  },
-  collapse: {
-    paddingRight: '15px',
-    paddingLeft: '15px',
-    overflowX: 'visible',
-    borderTopWidth: '1px',
-    borderTopStyle: 'solid',
-    borderColor: '#E7E7E7',
-    boxShadow: '0px 1px 0px rgba(255, 255, 255, .1) inset',
-    marginRight: '-15px',
-    marginLeft: '-15px',
-    maxHeight: '340px',
-    boxSizing: 'border-box',
-    display: 'none',
-
-    '@media (min-width: 768px)': {
-      marginRight: '0px',
-      marginLeft: '0px',
-      paddingRight: '0px',
-      paddingLeft: '0px',
-      height: 'auto',
-      paddingBottom: '0px',
-      display: 'block',
-      overflow: 'visible',
-      width: 'auto',
-      borderTopWidth: '0px',
-      borderTopStyle: 'none',
-      boxShadow: 'none',
-      overflowY: 'visible'
-    }
+    padding: '0'
   }
 };
 
@@ -80,11 +42,9 @@ class NavbarItems extends Component {
     const defStyle = this.getStyles();
     const { style } = this.props;
     return (
-      <div className="collapse" style={[defStyle.collapse]}>
-        <ul className="navitems" style={[defStyle.base, style && style]}>
-          {this.renderChildren()}
-        </ul>
-      </div>
+      <ul className="navitems" style={[defStyle.base, style && style]}>
+        {this.renderChildren()}
+      </ul>
     );
   }
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleRoot } from 'radium';
 import Nav from './nav';
+import '../css/styles.css';
 
 export default ({ children }) => (
   <StyleRoot>
