6c04c08fe5d19c1b33224092fa70194e8300f96e
Tom Dane <tjdane@gmail.com>
2019-02-05 18:11:19 -0800

    Demo

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <Header title="About" description="Ocius history and information" />
-    <h2>Mission</h2>
+    <h2>Hi kevin</h2>
     <h2>People</h2>
     <h2>Partners</h2>
     <h2>Awards</h2>
