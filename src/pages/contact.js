4ead25db680ab3782fd4c272c56df06df54198ee
Sergey Monin <moninsergei@gmail.com>
2019-02-08 02:58:22 +0400

    Add some basic layout to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PageLayoutSidebar from '../layouts/PageLayoutSidebar';
 import Header from '../components/header';
+import Container from '../components/Container';
+import Row from '../components/GridRow';
+import Column from '../components/GridColumn';
 
 export default () => (
   <PageLayoutSidebar>
     <Header title="Contact" description="Links to contact Ocius" />
-    <p>Email</p>
-    <p>Phone</p>
-    <p>Twitter</p>
-    <p>Facebook</p>
-    <p>Instagram</p>
+    <section className="page-content">
+      <Container>
+        <Row>
+          <Column className="primary-content" sm={8} md={7} lg={7} fluid>
+            <p>Email</p>
+            <p>Phone</p>
+            <p>Twitter</p>
+            <p>Facebook</p>
+            <p>Instagram</p>
+          </Column>
+          <Column className="secondary-content" sm={4} md={5} lg={4} lgShift={1} fluid>
+            Latest Posts
+          </Column>
+        </Row>
+      </Container>
+    </section>
   </PageLayoutSidebar>
 );
