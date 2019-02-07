135a6825da86a3261b9fa8cc7eba489a7bbd9bd1
Nolan Braman <nolan.braman@gmail.com>
2019-02-07 15:19:02 -0500

    commit 2, added graphql blog post data to contact pg

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/global.css';
 
-export default footer => (
+export default props => (
   <div>
     <div className="footer">
       <h1 className="footer-items">- Footer Placeholder - </h1>
