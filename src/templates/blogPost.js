3037adf0b4d92809554df5a202a8ecea9f4770ec
Sergey Monin <moninsergei@gmail.com>
2019-02-10 06:14:17 +0400

    Update blog post layout

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../layouts/Layout';
 import Container from '../components/Container';
+import Row from '../components/GridRow';
+import Column from '../components/GridColumn';
+import RecentNews from '../components/RecentNews';
 import Header from '../components/header';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
-      <div>
-        <Header title={post.frontmatter.title} description={post.excerpt} />
+      <Header title="News" description={post.excerpt} />
+      <section className="page-content">
         <Container>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <Row>
+            <Column className="primary-content" sm={8} md={7} lg={7} fluid>
+              <h2 className="title">{post.frontmatter.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </Column>
+            <Column className="secondary-content" sm={4} md={5} lg={4} lgShift={1} fluid>
+              <RecentNews />
+            </Column>
+          </Row>
         </Container>
-      </div>
+      </section>
     </Layout>
   );
 };
