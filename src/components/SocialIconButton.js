31ab896799b11dc877b2414781213f632071b488
Sergey Monin <moninsergei@gmail.com>
2019-02-16 11:42:10 +0400

    Add skeleton for sharing functionality (Icon + SocialIconButton components)

---
+++ src/components/SocialIconButton.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Radium from 'radium';
+import cn from 'classnames';
+import Icon from './Icon';
+
+const backgroundColors = {
+  email: '#2c3643',
+  facebook: '#3a5999',
+  facebookMessenger: '#1472fb',
+  pinterest: '#cb2027',
+  reddit: '#fc4220',
+  twitter: '#1da1f2',
+  whatsapp: '#28eb76',
+  weChat: '#7bb32e'
+};
+
+const iconNames = {
+  email: 'Email',
+  facebook: 'Facebook',
+  facebookMessenger: 'FacebookMessenger',
+  pinterest: 'Pinterest',
+  reddit: 'Reddit',
+  twitter: 'Twitter',
+  whatsapp: 'Whatsapp',
+  weChat: 'WeChat'
+};
+
+const sizeMultiplier = 2.5;
+
+const iconFromString = (iconName, props) => React.createElement(Icon[iconName], { ...props });
+
+const SocialIconButton = ({ network, href, onClick, iconSize, id, className, style }) => {
+  const size = iconSize * sizeMultiplier;
+
+  const hoverStyles = {
+    opacity: 0.7
+  };
+
+  const styles = {
+    backgroundColor: backgroundColors[network],
+    color: '#fff',
+    cursor: 'pointer',
+    display: 'inline-block',
+    fontSize: `${iconSize}px`,
+    height: `${size}px`,
+    lineHeight: `${size}px`,
+    textAlign: 'center',
+    textDecoration: 'none',
+    transition: `opacity 200ms ease-in-out`,
+    width: `${size}px`,
+
+    ':hover': hoverStyles,
+    ':active': hoverStyles
+  };
+
+  return (
+    <a
+      className={cn('SocialIconButton', className)}
+      id={id}
+      href={href}
+      onClick={onClick}
+      style={[styles, style]}
+      data-network={network}
+    >
+      {iconFromString(iconNames[network])}
+    </a>
+  );
+};
+
+SocialIconButton.propTypes = {
+  network: PropTypes.oneOf([
+    'email',
+    'facebook',
+    'facebookMessenger',
+    'pinterest',
+    'reddit',
+    'twitter',
+    'whatsapp',
+    'weChat'
+  ]).isRequired,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  iconSize: PropTypes.number,
+  id: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.object)
+};
+
+SocialIconButton.defaultProps = {
+  href: null,
+  onClick: null,
+  iconSize: 16,
+  id: '',
+  className: '',
+  style: {}
+};
+
+export default Radium(SocialIconButton);
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -6,6 +6,7 @@ import Row from '../components/GridRow';
 import Column from '../components/GridColumn';
 import RecentNews from '../components/RecentNews';
 import Header from '../components/header';
+import SocialIconButton from '../components/SocialIconButton';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
@@ -20,6 +21,20 @@ export default ({ data }) => {
               <p className="date">{post.frontmatter.date}</p>
               <p className="author">{post.frontmatter.author}</p>
               <div dangerouslySetInnerHTML={{ __html: post.html }} />
+              <div className="post-share">
+                <h5>Share this post:</h5>
+                <SocialIconButton network="email" href="mailto:?subject=&body=" />
+                <SocialIconButton
+                  network="twitter"
+                  href="https://twitter.com/intent/tweet?text=&url=&via="
+                />
+                <SocialIconButton
+                  network="facebook"
+                  href="https://www.facebook.com/sharer/sharer.php?u="
+                />
+                <SocialIconButton href="fb-messenger://share/?link=" network="facebookMessenger" />
+                <SocialIconButton network="reddit" href="http://www.reddit.com/submit/?url=" />
+              </div>
             </Column>
             <Column className="secondary-content" sm={4} md={5} lg={4} lgShift={1} fluid>
               <RecentNews />
