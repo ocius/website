ce7fbce239c2834ddb22c61ca7033b3770c5e984
Sergey Monin <moninsergei@gmail.com>
2019-02-17 05:22:03 +0400

    Add basic layout to blog index page

--- src/pages/news.js
+++ src/pages/news.js
@@ -2,21 +2,35 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Header from '../components/header';
 import Layout from '../layouts/Layout';
+import Container from '../components/Container';
+import Row from '../components/GridRow';
+import Column from '../components/GridColumn';
+import RecentNews from '../components/RecentNews';
 
 export default ({ data }) => {
   return (
     <Layout>
       <Header title="News" description="Ocius blog and news" />
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.fields.slug}>
-            <h3>
-              {node.frontmatter.title} <span> — {node.frontmatter.date} </span>
-            </h3>
-          </Link>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
+      <section className="page-content">
+        <Container>
+          <Row>
+            <Column className="primary-content" sm={8} md={7} lg={7} fluid>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <article key={node.id} className="post">
+                  <h3>
+                    <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+                  </h3>
+                  <p className="date">{node.frontmatter.date}</p>
+                  <p>{node.excerpt}</p>
+                </article>
+              ))}
+            </Column>
+            <Column className="secondary-content" sm={4} md={5} lg={4} lgShift={1} fluid>
+              <RecentNews />
+            </Column>
+          </Row>
+        </Container>
+      </section>
     </Layout>
   );
 };
@@ -30,7 +44,7 @@ export const query = graphql`
           id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "MMMM DD, YYYY")
           }
           fields {
             slug
