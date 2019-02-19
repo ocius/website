e6eb846ef79d6761f7bf19678ed7c50f2c07b1e6
Sergey Monin <moninsergei@gmail.com>
2019-02-19 10:39:50 +0400

    Fix image paths in blog posts (copy them to public folder)

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,22 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`,
+
+    // Parse all markdown files (each plugin add/parse some data into GraphQL layer)
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 690
+            }
+          },
+          `gatsby-remark-copy-linked-files`
+        ]
+      }
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`
