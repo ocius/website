d285235800f5ba21b00a0291958c49dcd48fab31
Sergey Monin <moninsergei@gmail.com>
2019-02-19 18:27:13 +0400

    Add new component - heading

---
+++ src/common/typography.js
@@ -0,0 +1,18 @@
+export const fontWeightLight = 300;
+export const fontWeightRegular = 500;
+export const fontWeightMedium = 600;
+
+export const fontSizeSuper = 88;
+export const fontSizeHeading1 = 64;
+export const fontSizeHeading2 = 48;
+export const fontSizeHeading3 = 40;
+export const fontSizeHeading4 = 32;
+export const fontSizeHeading5 = 24;
+export const fontSizeHeading6 = 20;
+export const fontSizeHeading7 = 16;
+export const fontSizeHeading8 = 11;
+export const fontSizeBodyArticle = 20;
+export const fontSizeBodyArticleSmall = 16;
+export const fontSizeBodySmall = 14;
+export const fontSizeAccent = 16;
+export const fontSizeUppercase = 11;
