2117cf20929bfd46d2c4059cc15f60fd3244c6f6
Sergey Monin <moninsergei@gmail.com>
2019-02-28 02:45:13 +0400

    Use GraphQL to query images on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
 import Container from '../components/Container';
@@ -12,10 +13,6 @@ import PanelCallout from '../components/PanelCallout';
 import PanelCalloutGroup from '../components/PanelCalloutGroup';
 import { add, gutter, span } from '../common/grid';
 import mq from '../common/mq';
-import HeroBackground from '../images/wallpaperocius-768x432.jpg';
-import ScienceTile from '../images/Science-Home-Tile-Background.jpg';
-import OilTile from '../images/Oil-Home-Tile-Background.jpg';
-import DefenceTile from '../images/Defence-Home-Tile-Background.jpg';
 
 const gutterWidth = 30;
 
@@ -44,25 +41,27 @@ const styles = {
   }
 };
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <SEO title="OCIUS - Satellites of the Sea" />
     <Segmented borderBottom="">
-      <HeroBlock
-        image={HeroBackground}
-        gradient="leftCorner"
-        style={{
-          height: 'calc(100vh - 4em)',
-          color: '#ffffff'
-        }}
-      >
-        <div className="HeroBlock-callout" style={styles.callout}>
-          <Heading level="1" size="huge" weight="thick">
-            Innovative Autonomous Solutions
-          </Heading>
-          <Heading level="3">For persistent maritime surveillance</Heading>
-        </div>
-      </HeroBlock>
+      {data.HeroBackground && (
+        <HeroBlock
+          image={data.HeroBackground.childImageSharp.sizes.src}
+          gradient="leftCorner"
+          style={{
+            height: 'calc(100vh - 4em)',
+            color: '#ffffff'
+          }}
+        >
+          <div className="HeroBlock-callout" style={styles.callout}>
+            <Heading level="1" size="huge" weight="thick">
+              Innovative Autonomous Solutions
+            </Heading>
+            <Heading level="3">For persistent maritime surveillance</Heading>
+          </div>
+        </HeroBlock>
+      )}
     </Segmented>
     <Container style={styles.alignment.center}>
       <Segmented borderBottom="">
@@ -89,24 +88,74 @@ export default () => (
 
     <Segmented borderBottom="">
       <PanelCalloutGroup>
-        <PanelCallout bgImage={DefenceTile} title="Defence" url="/defence">
-          <p>Anti Submarine Warfare</p>
-          <p>Electronic Warfare</p>
-          <p>Gateway Coms</p>
-          <p>Mine Counter Measures</p>
-        </PanelCallout>
+        {data.DefenceTile && (
+          <PanelCallout
+            bgImage={data.DefenceTile.childImageSharp.sizes.src}
+            title="Defence"
+            url="/defence"
+          >
+            <p>Anti Submarine Warfare</p>
+            <p>Electronic Warfare</p>
+            <p>Gateway Coms</p>
+            <p>Mine Counter Measures</p>
+          </PanelCallout>
+        )}
 
-        <PanelCallout bgImage={OilTile} title="Oil &amp; Gas" url="/oil-and-gas">
-          <p>Mapping pipelines</p>
-          <p>Security</p>
-          <p>Environmental monitoring</p>
-        </PanelCallout>
+        {data.OilTile && (
+          <PanelCallout
+            bgImage={data.OilTile.childImageSharp.sizes.src}
+            title="Oil &amp; Gas"
+            url="/oil-and-gas"
+          >
+            <p>Mapping pipelines</p>
+            <p>Security</p>
+            <p>Environmental monitoring</p>
+          </PanelCallout>
+        )}
 
-        <PanelCallout bgImage={ScienceTile} title="Science" url="/science">
-          <p>Environmental monitoring</p>
-          <p>Seabed Mapping</p>
-        </PanelCallout>
+        {data.ScienceTile && (
+          <PanelCallout
+            bgImage={data.ScienceTile.childImageSharp.sizes.src}
+            title="Science"
+            url="/science"
+          >
+            <p>Environmental monitoring</p>
+            <p>Seabed Mapping</p>
+          </PanelCallout>
+        )}
       </PanelCalloutGroup>
     </Segmented>
   </Layout>
 );
+
+export const query = graphql`
+  query getHomepageImages {
+    HeroBackground: file(relativePath: { eq: "images/wallpaperocius-768x432.jpg" }) {
+      childImageSharp {
+        sizes(quality: 100) {
+          src
+        }
+      }
+    }
+
+    ScienceTile: file(relativePath: { eq: "images/Science-Home-Tile-Background.jpg" }) {
+      ...imageSharpTile
+    }
+
+    OilTile: file(relativePath: { eq: "images/Oil-Home-Tile-Background.jpg" }) {
+      ...imageSharpTile
+    }
+
+    DefenceTile: file(relativePath: { eq: "images/Defence-Home-Tile-Background.jpg" }) {
+      ...imageSharpTile
+    }
+  }
+
+  fragment imageSharpTile on File {
+    childImageSharp {
+      sizes(quality: 100) {
+        src
+      }
+    }
+  }
+`;
