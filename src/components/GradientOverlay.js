db15906db75d0fea2235753b32d5b35f94597002
r007 <moninsergei@gmail.com>
2019-03-09 16:19:13 +0400

    Refactor GradientOverlay component, Radium -> Styled-components

--- src/components/GradientOverlay.js
+++ src/components/GradientOverlay.js
@@ -1,61 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import radium from 'radium';
-import assign from 'object-assign';
+import styled, { css } from 'styled-components';
 
 import rgb from '../common/color';
 
-const GradientOverlay = ({ children, gradientType, color, style }) => {
-  const styles = {
-    container: {
-      bottom: 0,
-      left: 0,
-      opacity: 0.15,
-      position: 'absolute',
-      right: 0,
-      top: 0,
-      zIndex: 1
-    },
+const GradientContainer = styled.div`
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  opacity: 0.15;
+  position: absolute;
+  z-index: 1;
 
-    type: {
-      linear: {
-        opacity: 0.45,
-        backgroundImage: `linear-gradient(-180deg, rgba(${rgb(color)}, 0) 53%, ${color} 100%)`
-      },
+  ${props =>
+    props.type === 'linear' &&
+    css`
+      opacity: 0.45;
+      background-image: linear-gradient(
+        -180deg,
+        rgba(${rgb(props.color)}, 0) 53%,
+        ${props.color} 100%
+      );
+    `}
 
-      radial: {
-        backgroundImage: `radial-gradient(55% 60%, rgba(${rgb(color)}, 0) 40%, ${color} 100%)`
-      },
+  ${props =>
+    props.type === 'radial' &&
+    css`
+      background-image: radial-gradient(
+        55% 60%,
+        rgba(${rgb(props.color)}, 0) 40%,
+        ${props.color} 100%
+      );
+    `}
+    
+  ${props =>
+    props.type === 'leftCorner' &&
+    css`
+      opacity: 0.67;
+      background-image: linear-gradient(
+        to right top,
+        ${props.color},
+        rgba(${rgb(props.color)}, 0) 56%
+      );
+    `}
+`;
 
-      leftCorner: {
-        opacity: 0.67,
-        backgroundImage: `linear-gradient(to right top, ${color}, rgba(${rgb(color)}, 0) 56%)`
-      }
-    }
-  };
+GradientContainer.defaultProps = {};
 
+const GradientOverlay = ({ children, gradientType, color }) => {
   return (
-    <div
-      className="GradientOverlay"
-      style={assign({}, styles.container, gradientType && styles.type[gradientType], style)}
-    >
+    <GradientContainer className="GradientOverlay" type={gradientType} color={color}>
       {children}
-    </div>
+    </GradientContainer>
   );
 };
 
 GradientOverlay.propTypes = {
   color: PropTypes.string,
   children: PropTypes.element,
-  gradientType: PropTypes.oneOf(['', 'linear', 'radial', 'leftCorner']),
-  style: PropTypes.objectOf(PropTypes.object)
+  gradientType: PropTypes.oneOf(['', 'linear', 'radial', 'leftCorner'])
 };
 
 GradientOverlay.defaultProps = {
   children: '',
   color: '#4cb4e6',
-  gradientType: 'linear',
-  style: {}
+  gradientType: 'linear'
 };
 
-export default radium(GradientOverlay);
+export default GradientOverlay;
