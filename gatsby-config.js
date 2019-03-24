800b71faebd30bf5e35c8c2f5a3ca3d64da3f0f2
r007 <moninsergei@gmail.com>
2019-03-24 05:47:46 +0400

    Update all the packages, remove CSP plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,23 +83,6 @@ module.exports = {
       }
     },
 
-    // Add sha256 hashes to scripts and styles to fix CSP issues
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: true, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-        directives: {
-          'script-src': "'self' www.google-analytics.com",
-          'style-src': "'self' 'unsafe-inline'",
-          'img-src': "'self' data: www.google-analytics.com"
-          // you can add your directives or override defaults
-        }
-      }
-    },
-
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
