7bef519a0358934dc2efa91d62708b44e1b4f2f9
Tom Dane <tdane@tesla.com>
2019-03-27 07:31:24 -0700

    Add adobe fonts to script CSP

--- security/originResponse.js
+++ security/originResponse.js
@@ -11,12 +11,10 @@ exports.handler = (event, context, callback) => {
   
   headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
   headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'self' ;" +
-  "script-src 'self' 'unsafe-inline';" +
-  "script-src-elem 'self' https://www.google-analytics.com;" +
+  "script-src 'self' 'unsafe-inline' www.google-analytics.com use.typekit.net;" +
   "style-src 'self' 'unsafe-inline';" +
   "img-src 'self' data:;" +
-  "font-src 'self' https://use.typekit.net;" +
-  "manifest-src 'self'; "}]; 
+  "font-src 'self' use.typekit.net;"}]; 
   headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
   headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]; 
   headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}]; 
