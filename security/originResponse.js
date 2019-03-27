eae4da895ab60244fbe536a6e6f35c27a4265eba
Tom Dane <tdane@tesla.com>
2019-03-27 07:58:16 -0700

    Add child source

--- security/originResponse.js
+++ security/originResponse.js
@@ -10,10 +10,11 @@ exports.handler = (event, context, callback) => {
   }
   
   headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
-  headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'self' www.youtube.com usvna.ocius.com.au;" +
+  headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'self';" +
   "script-src 'self' 'unsafe-inline' www.google-analytics.com use.typekit.net;" +
   "style-src 'self' 'unsafe-inline';" +
   "img-src 'self' p.typekit.net data:;" +
+  "child-src www.youtube.com usvna.ocius.com.au;" +
   "font-src 'self' use.typekit.net;"}]; 
   headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
   headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]; 
