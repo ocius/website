dfd7d35205eb9acae3ff9d20596a565621463c4b
Kevin <kevinwochan@gmail.com>
2019-05-03 23:50:11 +1000

    alignments and adding Andrew

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -63,7 +63,7 @@ export default ({ data }) => (
         </Heading>
       </header>
       <Segmented borderBottom="">
-        <Row className="primary-content">
+        <Row className="primary-content centered">
           <Col xs={12} md={4} lg={4}>
             {data.RobertDane && (
               <Img fixed={data.RobertDane.childImageSharp.fixed} alt="Robert Dane" />
@@ -73,19 +73,21 @@ export default ({ data }) => (
               <br />
               Chief Executive Officer
             </Heading>
-            <p>
-              Robert Dane is founder and CEO. In 1996, Robert, a passionate sailor and
-              environmentalist, invented and patented the &apos;solarsail&apos; and designed the
-              first ‘solarsailor’ prototype which won the Advanced Technology Boat Race in Canberra
-              in 1997.
-            </p>
-            <p>
-              The first commercial &apos;solarsailor&apos; ferry won the Australian Design Award of
-              the Year in 2001. Robert is Intel Environment Laureate and a WWF Future Maker
-              recipient and was recently honoured to be a finalist for the International Windship
-              Awards Lifetime Achievement Award. In 2017, Robert was awarded the Spitfire Memorial
-              Defence Association Fellowship.
-            </p>
+            <div className="left-align">
+              <p>
+                Robert Dane is founder and CEO. In 1996, Robert, a passionate sailor and
+                environmentalist, invented and patented the &apos;solarsail&apos; and designed the
+                first ‘solarsailor’ prototype which won the Advanced Technology Boat Race in Canberra
+                in 1997.
+              </p>
+              <p>
+                The first commercial &apos;solarsailor&apos; ferry won the Australian Design Award of
+                the Year in 2001. Robert is Intel Environment Laureate and a WWF Future Maker
+                recipient and was recently honoured to be a finalist for the International Windship
+                Awards Lifetime Achievement Award. In 2017, Robert was awarded the Spitfire Memorial
+                Defence Association Fellowship.
+              </p>
+            </div>
           </Col>
           <Col xs={12} md={4} lg={4}>
             {data.GeoffZuber && (
@@ -96,17 +98,19 @@ export default ({ data }) => (
               <br />
               Non-Executive Director
             </Heading>
-            <p>
-              Well respected for his strategic thinking and passion for business transformation
-              programs, Geoff has earned the admiration and respect of many C- level executives
-              within leading Australian organisations in both government and private sectors.
-            </p>
-            <p>
-              Part of the founding management team at Holocentric, a Canberra based, he draws upon
-              his extensive experience working in sales and in senior management roles for global
-              companies such as Honeywell, Microsoft and Cisco Systems to help steer the company to
-              success
-            </p>
+            <div className="left-align">
+              <p>
+                Well respected for his strategic thinking and passion for business transformation
+                programs, Geoff has earned the admiration and respect of many C- level executives
+                within leading Australian organisations in both government and private sectors.
+              </p>
+              <p>
+                Part of the founding management team at Holocentric, a Canberra based, he draws upon
+                his extensive experience working in sales and in senior management roles for global
+                companies such as Honeywell, Microsoft and Cisco Systems to help steer the company to
+                success
+              </p>
+            </div>
           </Col>
           <Col xs={12} md={4} lg={4}>
             {data.BobHawke && <Img fixed={data.BobHawke.childImageSharp.fixed} alt="Bob Hawke" />}
@@ -115,17 +119,21 @@ export default ({ data }) => (
               <br />
               Chairman of Advisory Board
             </Heading>
-            <p>
-              Hon Bob Hawke was Prime Minister of Australia from 1983 to 1991. He has high-level
-              industry and government relations in Australia, China, the USA, and Europe.
-            </p>
-            <p>He was Chairman of OCIUS (formerly Solar Sailor) for 12 years from 2002 - 2014.</p>
+            <div className="left-align">
+              <p>
+                Hon Bob Hawke was Prime Minister of Australia from 1983 to 1991. He has high-level
+                industry and government relations in Australia, China, the USA, and Europe.
+              </p>
+              <p>
+                He was Chairman of OCIUS (formerly Solar Sailor) for 12 years from 2002 - 2014.
+              </p>
+            </div>
           </Col>
         </Row>
       </Segmented>
       <Segmented borderBottom="">
-        <Row className="primary-content">
-          <Col xs={12} md={4} lg={4}>
+        <Row className="primary-content centered">
+          <Col xs={12} md={3} lg={3}>
             {data.LloydBreckenridge && (
               <Img fixed={data.LloydBreckenridge.childImageSharp.fixed} alt="Lloyd Breckenridge" />
             )}
@@ -134,21 +142,23 @@ export default ({ data }) => (
               <br />
               Chief Engineer
             </Heading>
-            <p>
-              Lloyd has over 30 years of experience as a software engineer working in varied roles
-              for both large organisations and small innovative companies in Australia and overseas.
-              The last 6 years has seen him primarily involved in autonomous vehicle development and
-              applications. Lloyd was a key member of Team Thunder, who successfully competed in the
-              2014 UAV Challenge Outback Rescue competition run by the CSIRO and Queensland
-              University of Technology.
-            </p>
-            <p>
-              He has had considerable sailing experience having started sailing at an early age on
-              Lake Macquarie. In his current role as Senior Systems Engineer for OCIUS Technology
-              Ltd he feels he has found his dream job.
-            </p>
+            <div className="left-align">
+              <p>
+                Lloyd has over 30 years of experience as a software engineer working in varied roles
+                for both large organisations and small innovative companies in Australia and overseas.
+                The last 6 years has seen him primarily involved in autonomous vehicle development and
+                applications. Lloyd was a key member of Team Thunder, who successfully competed in the
+                2014 UAV Challenge Outback Rescue competition run by the CSIRO and Queensland
+                University of Technology.
+              </p>
+              <p>
+                He has had considerable sailing experience having started sailing at an early age on
+                Lake Macquarie. In his current role as Senior Systems Engineer for OCIUS Technology
+                Ltd he feels he has found his dream job.
+              </p>
+            </div>
           </Col>
-          <Col xs={12} md={4} lg={4}>
+          <Col xs={12} md={3} lg={3}>
             {data.PeterWlodarczyk && (
               <Img fixed={data.PeterWlodarczyk.childImageSharp.fixed} alt="Peter Wlodarczyk" />
             )}
@@ -157,20 +167,22 @@ export default ({ data }) => (
               <br />
               Lead Engineer
             </Heading>
-            <p>
-              Graduating from the University of Newcastle with a BE (Computer Engineering), Peter
-              has over 20 years experience as a computer systems/software engineer in defense, IT
-              and medical device industries. For the last 15 years, Peter has worked for ResMed as a
-              software engineer, system engineer and researcher, innovating new products for sleep
-              apnea and respiratory failure.
-            </p>
-            <p>
-              He has developed a keen interest in UAVs, working with Lloyd in a team that
-              successfully completed the 2014 UAV Challenge Outback Rescue competition run by the
-              CSIRO and Queensland University of Technology.
-            </p>
+            <div className="left-align">
+              <p>
+                Graduating from the University of Newcastle with a BE (Computer Engineering), Peter
+                has over 20 years experience as a computer systems/software engineer in defense, IT
+                and medical device industries. For the last 15 years, Peter has worked for ResMed as a
+                software engineer, system engineer and researcher, innovating new products for sleep
+                apnea and respiratory failure.
+              </p>
+              <p>
+                He has developed a keen interest in UAVs, working with Lloyd in a team that
+                successfully completed the 2014 UAV Challenge Outback Rescue competition run by the
+                CSIRO and Queensland University of Technology.
+              </p>
+            </div>
           </Col>
-          <Col xs={12} md={4} lg={4}>
+          <Col xs={12} md={3} lg={3}>
             {data.TraceySharah && (
               <Img fixed={data.TraceySharah.childImageSharp.fixed} alt="Tracey Sharah" />
             )}
@@ -179,36 +191,38 @@ export default ({ data }) => (
               <br />
               Group Accountant & Company Secretary
             </Heading>
-            <p>
-              Tracey has over 25 years’ experience in corporate and public practice accounting,
-              business advisory, tax and superannuation. 17 years of this experience is in running
-              her own CPA Public Practice Accounting Firm.
-            </p>
-            <p>
-              Specialising in Company Secretarial and outsourced Corporate Financial Officer
-              engagements, Business Taxation, Accounting, Advisory and Self-Managed Superannuation
-              Funds. The practice is a 100% sustainable business, adopts the latest accounting and
-              tax technology apps and 90% paperless.
-            </p>
+            <div className="left-align">
+              <p>
+                Tracey has over 25 years’ experience in corporate and public practice accounting,
+                business advisory, tax and superannuation. 17 years of this experience is in running
+                her own CPA Public Practice Accounting Firm.
+              </p>
+              <p>
+                Specialising in Company Secretarial and outsourced Corporate Financial Officer
+                engagements, Business Taxation, Accounting, Advisory and Self-Managed Superannuation
+                Funds. The practice is a 100% sustainable business, adopts the latest accounting and
+                tax technology apps and 90% paperless.
+              </p>
+          </div>
           </Col>
-          {/*
           <Col xs={12} md={3} lg={3}>
-            {data.AndrewAtkin && (
-              <Img fixed={data.AndrewAtkin.childImageSharp.fixed} alt="David Saxelby" />
+            {data.AndrewAitken && (
+              <Img fixed={data.AndrewAitken.childImageSharp.fixed} alt="Andrew Aitken" />
             )}
             <Heading level={3} size="medium">
-              Andrew Atkin
+              Andrew Aitken
               <br />
               Ocius Corporate Lawyer
             </Heading>
-            <p>
-              Andrew practices principally in the areas of commercial law, succession and estate
-              planning. Andrew provides advice on the sale and purchase of businesses and companies,
-              corporate governance, risk management and a wide range of issues facing SME owners,
-              including intellectual property, shareholder agreements and commercial contracts.
-            </p>
+            <div className ="left-align">
+              <p>
+                Andrew practices principally in the areas of commercial law, succession and estate
+                planning. Andrew provides advice on the sale and purchase of businesses and companies,
+                corporate governance, risk management and a wide range of issues facing SME owners,
+                including intellectual property, shareholder agreements and commercial contracts.
+              </p>
+            </div>
           </Col>
-          */}
         </Row>
       </Segmented>
       <header id="partners" className="centered">
@@ -768,7 +782,7 @@ export const query = graphql`
       ...imageSharpAvatar
     }
 
-    AndrewAtkin: file(relativePath: { eq: "pages/about/Andrew-Atkin.png" }) {
+    AndrewAitken: file(relativePath: { eq: "pages/about/Andrew-Aitken.png" }) {
       ...imageSharpAvatar
     }
 
