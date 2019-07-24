58917dc8fe578b6545e3f7c56148f6432a727a8a
r007 <moninsergei@gmail.com>
2019-07-24 18:49:07 +0400

    Add some basic content to LeftNav

--- src/components/LeftNav/LeftNav.js
+++ src/components/LeftNav/LeftNav.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import classnames from 'classnames';
 
-import { SideNav, SideNavItems } from 'carbon-components-react/lib/components/UIShell';
+import { SideNav } from 'carbon-components-react/lib/components/UIShell';
 
 import NavContext from '../../common/context/NavContext';
 import { useWindowSize } from '../../common/hooks';
 
-const LeftNav = props => {
-  const { children, leftNavIsOpen, toggleNavState } = useContext(NavContext);
+const LeftNav = ({ children, homepage }) => {
+  const { leftNavIsOpen, toggleNavState } = useContext(NavContext);
   const windowSize = useWindowSize();
 
   if (windowSize.innerWidth > 1056 && !leftNavIsOpen) {
@@ -21,10 +21,10 @@ const LeftNav = props => {
       aria-label="Side navigation"
       className={classnames({
         'bx--side-nav--website': true,
-        'bx--side-nav--website--light': !props.homepage
+        'bx--side-nav--website--light': !homepage
       })}
     >
-      <SideNavItems>{children}</SideNavItems>
+      {children}
     </SideNav>
   );
 };
--- src/pages/live.js
+++ src/pages/live.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+  HeaderSideNavItems,
+  SideNavItems,
+  HeaderMenuItem,
+  HeaderMenu,
+  SideNavMenu,
+  SideNavMenuItem,
+  SideNavLink
+} from 'carbon-components-react/lib/components/UIShell';
+import { Fade16 } from '@carbon/icons-react';
 import SEO from '../components/SEO';
 import UIHeader from '../components/UIHeader';
 import LeftNav from '../components/LeftNav';
@@ -8,6 +18,42 @@ export default () => (
   <>
     <SEO title="Live" description="See where Bluebottles are at any time – LIVE." />
     <UIHeader />
-    <LeftNav>Testing</LeftNav>
+    <LeftNav>
+      <SideNavItems>
+        <HeaderSideNavItems hasDivider>
+          <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
+          <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
+          <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
+            <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
+            <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
+          </HeaderMenu>
+        </HeaderSideNavItems>
+        <SideNavMenu renderIcon={Fade16} title="Category title">
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+        </SideNavMenu>
+        <SideNavMenu renderIcon={Fade16} title="Category title">
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+        </SideNavMenu>
+        <SideNavMenu renderIcon={Fade16} title="Category title" isActive>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+          <SideNavMenuItem aria-current="page" href="javascript:void(0)">
+            Link
+          </SideNavMenuItem>
+          <SideNavMenuItem href="javascript:void(0)">Link</SideNavMenuItem>
+        </SideNavMenu>
+        <SideNavLink renderIcon={Fade16} href="javascript:void(0)">
+          Link
+        </SideNavLink>
+        <SideNavLink renderIcon={Fade16} href="javascript:void(0)">
+          Link
+        </SideNavLink>
+      </SideNavItems>
+    </LeftNav>
   </>
 );
