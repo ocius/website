3f13396ff8699a42affaf9aca998ac60096ba24b
r007 <moninsergei@gmail.com>
2019-07-31 20:18:00 +0400

    Styles refactor LeftNavWrapper SCSS -> styled-components

--- src/components/carbon/LeftNav/LeftNavWrapper.js
+++ src/components/carbon/LeftNav/LeftNavWrapper.js
@@ -1,18 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 import cx from 'classnames';
 
-import {
-  leftNavWrapper,
-  expanded as expandedStyles,
-  shouldHideHeader as shouldHideHeaderStyles
-} from './LeftNavWrapper.module.scss';
+const NavWrapper = styled.div`
+  transition: transform 0.15s cubic-bezier(0.2, 0.2, 0.38, 0.9), top 110ms;
+  width: 256px;
+  position: fixed;
+  top: 0px;
+  bottom: 0;
+  z-index: 5999;
+  scrollbar-width: none;
+  transform: translateX(-256px);
+
+  :global(.bx--side-nav) {
+    box-shadow: none !important;
+  }
+
+  :global(.bx--side-nav__navigation) {
+    padding-top: 0;
+    padding-bottom: 48px;
+  }
+
+  @media (min-width: 66rem) {
+    transform: translateX(0px);
+  }
+
+  &.expanded {
+    transform: translateX(0px);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+
+    @media (min-width: 66rem) {
+      box-shadow: none;
+    }
+  }
+
+  &.shouldHideHeader {
+    top: -48px;
+  }
+`;
 
 const LeftNavWrapper = ({ expanded, shouldHideHeader, ...rest }) => {
-  const className = cx(leftNavWrapper, {
-    [expandedStyles]: expanded,
-    [shouldHideHeaderStyles]: shouldHideHeader
+  const className = cx({
+    expanded,
+    shouldHideHeader
   });
-  return <div className={className} {...rest} />;
+  return <NavWrapper className={className} {...rest} />;
 };
 
 export default LeftNavWrapper;
