55a0dacae65e5309caef9f584808c833744bc9ba
r007 <moninsergei@gmail.com>
2019-08-18 09:53:20 +0400

    Remove CORS proxy from Configuration class

--- src/common/api/configuration.js
+++ src/common/api/configuration.js
@@ -1,9 +1,6 @@
-// Watch this, here goes the real magic
-const proxy = 'https://cors-anywhere.herokuapp.com/';
-
 class Configuration {
   // API to get drones locations
-  DRONE_COLLECTION_URL = `${proxy}https://api.ocius.com.au/drones`;
+  DRONE_COLLECTION_URL = `https://api.ocius.com.au/drones`;
 }
 
 export default Configuration;
