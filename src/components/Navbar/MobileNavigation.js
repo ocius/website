799741ac706ce7c18c1b9f12d2bfae5140e4ee38
r007 <moninsergei@gmail.com>
2019-10-07 12:02:59 +0400

    Refactor navbar components

--- src/components/Navbar/MobileNavigation.js
+++ src/components/Navbar/MobileNavigation.js
@@ -62,15 +62,13 @@ const MobileNavItem = ({ linkTo, label, icon }) => (
 );
 
 const MobileNavigation = () => (
-  <>
-    <Wrapper>
-      <MobileNavItem linkTo="/" label="Home" icon="Home" />
-      <MobileNavItem linkTo="/about" label="About" icon="Idea" />
-      <MobileNavItem linkTo="/live" label="Live" icon="Globe" />
-      <MobileNavItem linkTo="/usv" label="Drones" icon="Drone" />
-      <MobileNavItem linkTo="/news" label="News" icon="News" />
-    </Wrapper>
-  </>
+  <Wrapper>
+    <MobileNavItem linkTo="/" label="Home" icon="Home" />
+    <MobileNavItem linkTo="/about" label="About" icon="Idea" />
+    <MobileNavItem linkTo="/live" label="Live" icon="Globe" />
+    <MobileNavItem linkTo="/usv" label="Drones" icon="Drone" />
+    <MobileNavItem linkTo="/news" label="News" icon="News" />
+  </Wrapper>
 );
 
 export default MobileNavigation;
