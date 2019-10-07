24b3549f89c79b0c847d419784fdec6900d213c6
r007 <moninsergei@gmail.com>
2019-10-07 08:23:29 +0400

    Add detection for mobile device in useOnClickOutside function

--- src/common/hooks/useOnClickOutside.js
+++ src/common/hooks/useOnClickOutside.js
@@ -2,15 +2,19 @@ import { useEffect } from 'react';
 
 const useOnClickOutside = (ref, handler) => {
   useEffect(() => {
+    // Detect if it's a mobile device
+    const isTouchDevice = 'ontouchstart' in document.documentElement;
+    const eventListener = isTouchDevice ? 'touchstart' : 'mousedown';
+
     const listener = event => {
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
       handler(event);
     };
-    document.addEventListener('mousedown', listener);
+    document.addEventListener(eventListener, listener);
     return () => {
-      document.removeEventListener('mousedown', listener);
+      document.removeEventListener(eventListener, listener);
     };
   }, [ref, handler]);
 };
