784aaa320451dea9732e29f98af492fca346e4c0
r007 <moninsergei@gmail.com>
2019-10-11 01:05:45 +0400

    Better footer on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,167 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Col, Row } from 'react-flexbox-grid';
+import { Link } from 'gatsby';
+import mq from '../common/mq';
+import Icon from './Icon';
+import SocialLink from './SocialLink';
+import Container from './Container';
 
 const StyledFooter = styled.footer`
   background-color: #125192;
   width: 100%;
   clear: both;
   padding: 4.5em 0;
+
+  @media (max-width: ${mq.max[768]}) {
+    padding: 2em 0;
+
+    // Three columns in footer, separated by delimiter
+    .separated-section {
+      padding-bottom: 0.8em;
+
+      & + .separated-section {
+        border-left: 1px solid rgba(255, 255, 255, 0.3);
+      }
+    }
+
+    // Full-size section in footer
+    .full-section {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      border-top: 1px solid rgba(255, 255, 255, 0.3);
+      border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+    }
+  }
+
+  .footer-info {
+    text-align: right;
+
+    @media (max-width: ${mq.max[768]}) {
+      padding: 1.6em 0 2em;
+      display: flex;
+      flex-direction: row-reverse;
+
+      .info {
+        text-align: left;
+        p {
+          margin-top: 0;
+        }
+      }
+    }
+  }
 `;
 
-const Footer = ({ children, footerStyle }) => {
-  return (
-    <StyledFooter className="footer" style={footerStyle}>
-      {children}
-    </StyledFooter>
-  );
-};
+const LinkStyles = css`
+  display: block;
+  padding: 8px 0;
+  font-size: 17px;
+`;
+
+const FooterLink = styled(Link)`
+  ${LinkStyles}
+`;
+
+const Footer = ({ children, footerStyle }) => (
+  <StyledFooter className="footer" style={footerStyle}>
+    {children}
+  </StyledFooter>
+);
 
 Footer.propTypes = {
   footerStyle: PropTypes.objectOf(PropTypes.object),
-  children: PropTypes.node.isRequired
+  children: PropTypes.node
 };
 
 Footer.defaultProps = {
-  footerStyle: {}
+  footerStyle: {},
+  children: (
+    <Container>
+      <Row>
+        <Col className="separated-section" xs={4} md={2} lg={2}>
+          <FooterLink to="/about">About</FooterLink>
+          <ul className="menu-secondary">
+            <li>
+              <FooterLink to="/about">Mission</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/about#people">People</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/about#partners">Partners</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/about#story">Our Story</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/about#awards">Awards</FooterLink>
+            </li>
+          </ul>
+        </Col>
+        <Col className="separated-section" xs={4} md={2} lg={2}>
+          <FooterLink to="/#solutions">Solutions</FooterLink>
+          <ul className="menu-secondary">
+            <li>
+              <FooterLink to="/defence">Defence</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/oil-and-gas">Oil & Gas</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/science">Science</FooterLink>
+            </li>
+          </ul>
+        </Col>
+        <Col className="separated-section" xs={4} md={2} lg={2}>
+          <FooterLink to="/usv">Drones</FooterLink>
+          <ul className="menu-secondary">
+            <li>
+              <FooterLink to="/usv">USV</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/usv#overview">BlueBottle</FooterLink>
+            </li>
+            <li>
+              <FooterLink to="/usv#technical">Tech Specs</FooterLink>
+            </li>
+          </ul>
+        </Col>
+        <Col className="full-section" xs={12} md={2} lg={2}>
+          <FooterLink to="/live">Live</FooterLink>
+          <FooterLink to="/news">News</FooterLink>
+          <FooterLink to="/contact">Contact</FooterLink>
+          <FooterLink to="/careers">Careers</FooterLink>
+        </Col>
+        <Col className="footer-info" xs={12} md={4} lg={3} lgOffset={1}>
+          <Icon.OciusGlobe
+            fill="#ffffff"
+            style={{
+              height: '110px',
+              marginBottom: '3px',
+              width: 'auto'
+            }}
+          />
+          <div className="info">
+            <p>
+              22 King St
+              <br />
+              Building R13
+              <br />
+              UNSW Randwick Campus
+              <br />
+              Randwick NSW 2031
+              <br />
+              Mail PO Box 4304 Castlecrag 2068 NSW Australia
+            </p>
+            <SocialLink network="facebook" href="https://www.facebook.com/ociustechnology" />
+            <SocialLink network="twitter" href="https://twitter.com/OciusTechnology" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
 };
 
 export default Footer;
