91f95df3db4d6ceea5f44c2e1ead27e969acf3aa
r007 <moninsergei@gmail.com>
2019-10-18 21:12:44 +0400

    Fix ESLint error

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../css/styles.css';
 
 export default ({ children }) => (
-  <Fragment>
+  <>
     <Navbar />
     {children}
     <Footer />
-  </Fragment>
+  </>
 );
