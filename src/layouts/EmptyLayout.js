bedf216613525379d94de10ec7acdc77b5a28d55
r007 <moninsergei@gmail.com>
2019-10-18 21:11:17 +0400

    Fix ESLint errors

--- src/layouts/EmptyLayout.js
+++ src/layouts/EmptyLayout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import '../css/styles.css';
 
-export default ({ children }) => <Fragment>{children}</Fragment>;
+export default ({ children }) => <>{children}</>;
