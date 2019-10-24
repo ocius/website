ba66c77ca8fabd32eb0efdf0d7983c49d630b9fc
Tom Dane <tdane@tesla.com>
2019-10-23 20:36:19 -0700

    Add service worker removal code to layout js

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -10,3 +10,9 @@ export default ({ children }) => (
     <Footer />
   </>
 );
+
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(r => r.unregister())
+  })
+}
