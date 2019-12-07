9d3303ed50d96c0701e6806d9006cfda1110ac26
r007 <moninsergei@gmail.com>
2019-12-07 15:47:22 +0400

    Get rid of @carbon/react-icons dependency

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -303,6 +303,18 @@ const Sliders = props => (
   </Icon>
 );
 
+const ChevronDownGlyph = props => (
+  <Icon {...props} viewBox="0 0 10 6">
+    <path d="M5 6L0 1 .7.3 5 4.6 9.3.3l.7.7z" />
+  </Icon>
+);
+
+const Close20 = props => (
+  <Icon {...props} viewBox="0 0 32 32">
+    <path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4l6.6 6.6L8 22.6 9.4 24l6.6-6.6 6.6 6.6 1.4-1.4-6.6-6.6L24 9.4z" />
+  </Icon>
+);
+
 export default {
   Email,
   Facebook,
@@ -335,5 +347,7 @@ export default {
   Contact,
   Drone,
   Globe,
-  Sliders
+  Sliders,
+  ChevronDownGlyph,
+  Close20
 };
