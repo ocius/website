35e87d2bb9a7a3e3ebcc6a5f26ad79fac4775e19
kevinwochan <kevinwochan@gmail.com>
2020-01-18 10:12:20 +1100

    added verbose SEO descriptions with USV

--- src/pages/defence.js
+++ src/pages/defence.js
@@ -18,7 +18,7 @@ export default ({ data }) => (
   <Layout>
     <SEO
       title="Defence"
-      description="Persistent USVs are being seen as strategic in Defence as force multipliers Bluebottles have more
+      description="Persistent Unmanned Surface Vessels (USVs) are being seen as strategic in Defence as force multipliers Bluebottles have more
        power payload and performance making them the superior persistent USV for defence"
     />
 
