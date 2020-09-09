3639c0f9f935cbd033f6f42135d2cbb51847aa3b
kevinwochan <kevinwochan@gmail.com>
2020-09-09 16:33:31 +1000

    some styling fixes

--- src/components/Map/TemperatureScale.js
+++ src/components/Map/TemperatureScale.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-import VSBar from './VerticalStackedBar'
+import VSBar from './VerticalStackedBar';
 
 const ChartBarWrapper = styled.div`
-    position: absolute;
-    right: 0;
-    height: 100%;
-    z-index: 1;
-    background: white;
+  position: absolute;
+  right: 0;
+  height: 100%;
+  z-index: 1;
+  background: white;
 `;
 
 const TemperatureScale = () => {
-    return (
-        <ChartBarWrapper>
-            <VSBar
-                showTextDown
-                data={[
-                    { value: 1, description: "28\u2103", color: "#ff9bff" },
-                    { value: 1, description: "27\u2103", color: '#ff00ff' },
-                    { value: 1, description: "26\u2103", color: '#FF8000' },
-                    { value: 1, description: "25\u2103", color: '#FF0000' },
-                    { value: 1, description: "24\u2103", color: '#ea3d25' },
-                    { value: 1, description: "23\u2103", color: '#ff6600' },
-                    { value: 1, description: "22\u2103", color: '#ffab01' },
-                    { value: 1, description: "21\u2103", color: '#ffff00' },
-                    { value: 1, description: "20\u2103", color: '#b5ff45' },
-                    { value: 1, description: "19\u2103", color: '#79b831' },
-                    { value: 1, description: "18\u2103", color: '#00aa00' },
-                    { value: 1, description: "17\u2103", color: '#018c64' },
-                    { value: 1, description: "16\u2103", color: '#00c3ac' },
-                    { value: 1, description: "15\u2103", color: '#00ffff' },
-                    { value: 1, description: "14\u2103", color: '#348fff' },
-                    { value: 1, description: "13\u2103", color: '#0245ff' },
-                    { value: 1, description: "12\u2103", color: '#33007f' },
-                    { value: 1, description: "11\u2103", color: '#800080' },
-                    { value: 1, description: "10\u2103", color: '#805880' }
-                ]}
-            />
-        </ChartBarWrapper>);
-}
+  return (
+    <ChartBarWrapper>
+      <VSBar
+        showTextDown
+        data={[
+          { value: 1, description: '28\u2103', color: '#ff9bff' },
+          { value: 1, description: '27\u2103', color: '#ff00ff' },
+          { value: 1, description: '26\u2103', color: '#FF8000' },
+          { value: 1, description: '25\u2103', color: '#FF0000' },
+          { value: 1, description: '24\u2103', color: '#ea3d25' },
+          { value: 1, description: '23\u2103', color: '#ff6600' },
+          { value: 1, description: '22\u2103', color: '#ffab01' },
+          { value: 1, description: '21\u2103', color: '#ffff00' },
+          { value: 1, description: '20\u2103', color: '#b5ff45' },
+          { value: 1, description: '19\u2103', color: '#79b831' },
+          { value: 1, description: '18\u2103', color: '#00aa00' },
+          { value: 1, description: '17\u2103', color: '#018c64' },
+          { value: 1, description: '16\u2103', color: '#00c3ac' },
+          { value: 1, description: '15\u2103', color: '#00ffff' },
+          { value: 1, description: '14\u2103', color: '#348fff' },
+          { value: 1, description: '13\u2103', color: '#0245ff' },
+          { value: 1, description: '12\u2103', color: '#33007f' },
+          { value: 1, description: '11\u2103', color: '#800080' },
+          { value: 1, description: '10\u2103', color: '#805880' },
+        ]}
+      />
+    </ChartBarWrapper>
+  );
+};
 
 export default TemperatureScale;
--- src/components/Map/VerticalStackedBar.js
+++ src/components/Map/VerticalStackedBar.js
@@ -1,188 +1,189 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import _ from "lodash";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 export default class VeritcalBar extends Component {
-    constructor(props) {
-        super(props);
-        //
-        this.state = {
-            listBars: [],
-            data: this.props.data
-        };
-        this.onClick = this.onClick.bind(this);
+  constructor(props) {
+    super(props);
+    //
+    this.state = {
+      listBars: [],
+      data: this.props.data,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState((state) => ({
+      ...state,
+      listBars: this.getListBarWithOtherParameters(),
+      data: this.props.data,
+    }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!_.isEqual(this.props, prevProps)) {
+      this.setState((state) => ({
+        ...state,
+        listBars: this.getListBarWithOtherParameters(),
+        data: this.props.data,
+      }));
     }
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (!_.isEqual(this.props, prevProps)) {
-            this.setState(state => ({
-                ...state,
-                listBars: this.getListBarWithOtherParameters(),
-                data: this.props.data
-            }));
-        }
+
+  /**
+   * Calculate the height of each bar
+   */
+  calcHeightTotal() {
+    let heightTotal = 0;
+    this.props.data.forEach((bar) => {
+      heightTotal += bar.value;
+    });
+    return heightTotal;
+  }
+
+  /**
+   * Returns the same list of bars with position and barHeight
+   */
+  getListBarWithOtherParameters() {
+    const heightTotal = this.calcHeightTotal();
+    let position = (this.props.outlineHeight * 2) / 100;
+    let barHeight = 0;
+    const listBars = this.props.data.map((bar) => {
+      position += barHeight;
+      barHeight =
+        (bar.value * 100) / heightTotal - (this.props.outlineHeight * 100) / bar.value / 100;
+      bar = Object.assign(
+        { position: position, barHeight: barHeight, heightTotal: heightTotal },
+        bar
+      );
+      return bar;
+    });
+    return listBars;
+  }
+
+  /**
+   * OnClick Event
+   */
+  onClick(evt, bar) {
+    Object.assign(evt, { bar: bar });
+    if (this.props.onClick) {
+      this.props.onClick(evt);
     }
+  }
 
-    componentDidMount() {
-        this.setState(state => ({
-            ...state,
-            listBars: this.getListBarWithOtherParameters(),
-            data: this.props.data
-        }));
-    }
+  /**
+   * Returns a new calculated rgb color
+   */
+  randomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
 
-    /**
-     * Calculate the height of each bar
-     */
-    calcHeightTotal() {
-        let heightTotal = 0;
-        this.props.data.forEach(bar => {
-            heightTotal = heightTotal + bar.value;
-        });
-        return heightTotal;
-    }
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 
-    /**
-     * Returns the same list of bars with position and barHeight
-     */
-    getListBarWithOtherParameters() {
-        const heightTotal = this.calcHeightTotal();
-        let position = (this.props.outlineHeight * 2) / 100;
-        let barHeight = 0;
-        const listBars = this.props.data.map(bar => {
-            position = position + barHeight;
-            barHeight =
-                (bar.value * 100) / heightTotal -
-                (this.props.outlineHeight * 100) / bar.value / 100;
-            bar = Object.assign(
-                { position: position, barHeight: barHeight, heightTotal: heightTotal },
-                bar
-            );
-            return bar;
-        });
-        return listBars;
-    }
+  /**
+   * Returns a list of texts of the bars into a div component
+   */
+  getListTextBar(showTextInsteadValue) {
+    const { showTextWithValue } = this.props;
+    const listText = this.state.listBars.map((bar, index) => {
+      return (
+        <div
+          key={index}
+          style={{
+            position: 'relative',
+            height: `${bar.barHeight}%`,
+            fontSize: '90%',
+          }}
+          onClick={(e) => this.onClick(e, bar)}
+        >
+          {showTextInsteadValue && bar.name}
+          {showTextInsteadValue && bar.name && showTextWithValue ? ': ' : ''}
+          {(!showTextInsteadValue || showTextWithValue) && (bar.description || bar.value || '')}
+        </div>
+      );
+    });
+    return listText;
+  }
 
-    /**
-     * OnClick Event
-     */
-    onClick(evt, bar) {
-        Object.assign(evt, { bar: bar });
-        if (this.props.onClick) {
-            this.props.onClick(evt);
-        }
-    }
+  renderBars() {
+    const listBars = [];
 
-    /**
-     * Returns a new calculated rgb color
-     */
-    randomColor() {
-        const r = Math.floor(Math.random() * 255);
-        const g = Math.floor(Math.random() * 255);
-        const b = Math.floor(Math.random() * 255);
-
-        return `rgb(${r}, ${g}, ${b})`;
-    }
-
-    /**
-     * Returns a list of texts of the bars into a div component
-     */
-    getListTextBar(showTextInsteadValue) {
-        const { showTextWithValue } = this.props;
-        const listText = this.state.listBars.map((bar, index) => {
-            return (
-                <div
-                    key={index}
-                    style={{
-                        position: "relative",
-                        height: `${bar.barHeight}%`,
-                        fontSize: "90%",
-                    }}
-                    onClick={e => this.onClick(e, bar)}
-                >
-                    {showTextInsteadValue && bar.name}
-                    {showTextInsteadValue && bar.name && showTextWithValue ? ": " : ""}
-                    {(!showTextInsteadValue || showTextWithValue) &&
-                        (bar.description || bar.value || "")}
-                </div>
-            );
-        });
-        return listText;
-    }
-
-    renderBars() {
-        const listBars = [];
-
-        listBars.push(
-            this.state.listBars.map((bar, index) => {
-                return (
-                    <g key={index} onClick={e => this.onClick(e, bar)}>
-                        <rect
-                            width={this.props.width}
-                            height={`${bar.barHeight + 0.1}%`}
-                            style={{
-                                fill: bar.color || this.randomColor(),
-                                strokeWidth: `${(this.props.outlineHeight * 100) /
-                                    (bar.widthTotal / this.props.data.length)}%`,
-                                stroke: this.props.outlineColor
-                            }}
-                            y={`${bar.position}%`}
-                        />
-                    </g>
-                );
-            })
-        );
-        return listBars;
-    }
-
-    /** 
-     * Always renders label on the left of the bar
-     */
-    renderLabel(showTextInsteadValue) {
+    listBars.push(
+      this.state.listBars.map((bar, index) => {
         return (
-            <div
-                id={`${this.props.id}_text`}
-                style={{
-                    textAlign: "left",
-                    display: "flex",
-                    height: "100%",
-                    flexDirection: "column",
-                    float: "left",
-                    padding: "0px 5px"
-                }}
-            >
-                {this.getListTextBar(showTextInsteadValue)}
-            </div>
+          <g key={index} onClick={(e) => this.onClick(e, bar)}>
+            <rect
+              width={this.props.width}
+              height={`${bar.barHeight + 0.1}%`}
+              style={{
+                fill: bar.color || this.randomColor(),
+                strokeWidth: `${
+                  (this.props.outlineHeight * 100) / (bar.widthTotal / this.props.data.length)
+                }%`,
+                stroke: this.props.outlineColor,
+              }}
+              y={`${bar.position}%`}
+            />
+          </g>
         );
-    }
+      })
+    );
+    return listBars;
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <svg id={this.props.id} width={this.props.width} height="100%">
-                    {this.renderBars()}
-                </svg>
-                {this.renderLabel(this.props.showTextDown)}
-            </React.Fragment>
-        );
-    }
+  /**
+   * Always renders label on the left of the bar
+   */
+  renderLabel(showTextInsteadValue) {
+    return (
+      <div
+        id={`${this.props.id}_text`}
+        style={{
+          textAlign: 'left',
+          display: 'flex',
+          height: '100%',
+          flexDirection: 'column',
+          float: 'left',
+          padding: '0px 5px',
+        }}
+      >
+        {this.getListTextBar(showTextInsteadValue)}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <svg id={this.props.id} width={this.props.width} height="100%">
+          {this.renderBars()}
+        </svg>
+        {this.renderLabel(this.props.showTextDown)}
+      </>
+    );
+  }
 }
 
 VeritcalBar.propTypes = {
-    data: PropTypes.array.isRequired,
-    id: PropTypes.string,
-    width: PropTypes.number,
-    fontColor: PropTypes.string,
-    onClick: PropTypes.func,
-    outlineHeight: PropTypes.number,
-    outlineColor: PropTypes.string
+  data: PropTypes.array.isRequired,
+  width: PropTypes.number,
+  showTextWithValue: true,
+  fontColor: PropTypes.string,
+  onClick: PropTypes.func,
+  outlineHeight: PropTypes.number,
+  outlineColor: PropTypes.string,
+  id: PropTypes.string,
 };
 
 VeritcalBar.defaultProps = {
-    width: 30,
-    showTextWithValue: true,
-    outlineHeight: 0,
-    outlineColor: "black",
-    fontColor: "white",
-    id: "vsbar"
+  width: 30,
+  showTextWithValue: true,
+  outlineHeight: 0,
+  outlineColor: 'black',
+  fontColor: 'white',
+  id: 'vsbar',
 };
