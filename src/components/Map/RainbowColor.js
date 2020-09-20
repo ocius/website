436b4e86d865a447b40e46c7c446687f422efaba
kevinwochan <kevinwochan@gmail.com>
2020-09-20 10:26:12 +1000

    fixing color psectrum

--- src/components/Map/RainbowColor.js
+++ src/components/Map/RainbowColor.js
@@ -1,6 +1,8 @@
 const RainbowColor = (currentTemperature) => {
-  const i = (currentTemperature * 255) / 19;
-  return `hsl(${i}, 100%, 50%)`;
+  console.log(currentTemperature);
+  let i = (currentTemperature * 255) / 18;
+  if (currentTemperature > 28) i = (28 * 255) / 18;
+  return `hsl(${28 - i}, 100%, 50%)`;
 };
 
 export default RainbowColor;
