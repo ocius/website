dfdf5e5c6351f3c3807a4ae2c41fb894faeee052
kevinwochan <kevinwochan@gmail.com>
2020-09-28 09:33:22 +1000

    using demo endpoint

--- src/common/api/configuration.js
+++ src/common/api/configuration.js
@@ -1,4 +1,4 @@
 export default {
   // API to get drones locations
-  DRONE_COLLECTION_URL: `https://api.ocius.com.au/drones`,
+  DRONE_COLLECTION_URL: `https://270589351ac101bf27bc70fe09fdfd06.m.pipedream.net`,
 };
