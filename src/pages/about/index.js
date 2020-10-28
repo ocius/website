0ddd635e3e95dad343881b5f54edc42f347019ce
r007 <moninsergei@gmail.com>
2020-10-28 05:56:09 +0400

    Add top wave to accent row on about/people page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -652,6 +652,21 @@ const LogoBackgroundWrapper = styled.div`
 
 const AccentRowWrapper = styled.div`
   background-color: #edf7fc;
+  overflow: hidden;
+
+  &:before {
+    content: '';
+    position: relative;
+    left: 0;
+    top: 0;
+    float: left;
+    display: block;
+    height: 4rem;
+    width: 100vw;
+    background: hsla(0, 0%, 100%, 0.5);
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 70 500 60' preserveAspectRatio='none'%3E%3Crect x='0' y='0' width='500' height='500' style='stroke: none; fill: %23edf7fc;' /%3E%3Cpath d='M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z' style='stroke: none; fill: %23ffffff;'%3E%3C/path%3E%3C/svg%3E");
+    background-size: 100% 100%;
+  }
 `;
 
 const TeamMember = styled.div`
