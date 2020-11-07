b317032f018c7e4d1ce169b570199859f3c33a16
r007 <moninsergei@gmail.com>
2020-11-07 17:30:48 +0400

    Move contact block to about component

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1037,52 +1037,6 @@ const People = ({ data }) => {
             </Row>
           </Container>
         </AccentRowWrapper>
-
-        <Container className="page-content">
-          <Heading level={3} color="#36BBE7" size="large" weight="thick" underline="left">
-            Contact Us
-          </Heading>
-          <Row>
-            <Col xs={12} md={3} lg={3}>
-              <p>
-                <strong>Ocius Headquarters</strong>
-                <br />
-                Building R13
-                <br />
-                UNSW Randwick Campus
-                <br />
-                22 King St, Randwick NSW 2031
-              </p>
-            </Col>
-            <Col xs={12} md={3} lg={3}>
-              <p>
-                <strong>Postal Address</strong>
-                <br />
-                Mail PO Box 4304 Castlecrag
-                <br />
-                NSW 2068
-              </p>
-            </Col>
-            <Col xs={12} md={3} lg={3}>
-              <p>
-                <strong>General Enquiries:</strong>
-                <br />
-                +61 2 9924 6400
-                <br />
-                contact@ocius.com.au
-              </p>
-            </Col>
-            <Col xs={12} md={3} lg={3}>
-              <p>
-                <strong>Open Hours:</strong>
-                <br />
-                Mon - Fri: 9am - 5pm
-                <br />
-                Sat - Sun: Closed
-              </p>
-            </Col>
-          </Row>
-        </Container>
       </Segmented>
     </>
   );
@@ -1166,6 +1120,51 @@ export default ({ data }) => {
         <Awards path="about/awards" data={data} />
         <OurStory path="about/our-story" data={data} />
       </Router>
+      <Container className="page-content">
+        <Heading level={3} color="#36BBE7" size="large" weight="thick" underline="left">
+          Contact Us
+        </Heading>
+        <Row>
+          <Col xs={12} md={3} lg={3}>
+            <p>
+              <strong>Ocius Headquarters</strong>
+              <br />
+              Building R13
+              <br />
+              UNSW Randwick Campus
+              <br />
+              22 King St, Randwick NSW 2031
+            </p>
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+            <p>
+              <strong>Postal Address</strong>
+              <br />
+              Mail PO Box 4304 Castlecrag
+              <br />
+              NSW 2068
+            </p>
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+            <p>
+              <strong>General Enquiries:</strong>
+              <br />
+              +61 2 9924 6400
+              <br />
+              contact@ocius.com.au
+            </p>
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+            <p>
+              <strong>Open Hours:</strong>
+              <br />
+              Mon - Fri: 9am - 5pm
+              <br />
+              Sat - Sun: Closed
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
 };
