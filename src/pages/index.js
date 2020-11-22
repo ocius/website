a68c734935b7994019568bb51802e29c803fc08a
r007 <moninsergei@gmail.com>
2020-11-22 06:54:37 +0400

    Add 'read more' links to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Loadable from '@loadable/component';
 import { Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import ReadMoreReact from 'read-more-react';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
 import Container from '../components/Container';
@@ -141,15 +142,20 @@ export default ({ data }) => {
                 <Heading level={3} size="medium">
                   Solar Sail
                 </Heading>
-                <p>
-                  When deployed, the patented hard solar sail harnesses both solar and wind energy.
+                <ReadMoreReact
+                  text="When deployed, the patented hard solar sail harnesses both solar and wind energy.
                   When not desired it automatically folds snuggly into the deck of the Bluebottle
                   like the wing of a bird, so 100% retracted but also not taking up precious payload
                   space within the vessel. Intelligent programming means the solar sail reacts
                   autonomously to the sea, sun &amp; wind conditions to ensure efficient and safe
-                  operation for all mission requirements.
-                </p>
+                  operation for all mission requirements."
+                  min={140}
+                  ideal={160}
+                  max={180}
+                  readMoreText="Read more"
+                />
 
+                <Spacing $value="40px" />
                 <Icon.RudderCurrent
                   fill="#0fb0e1"
                   style={{
@@ -168,15 +174,19 @@ export default ({ data }) => {
                 <Heading level={3} size="medium">
                   Rudder Flipper
                 </Heading>
-                <p>
-                  The patented rudder-flipper steers, guides and powers the unmanned vessel
+                <ReadMoreReact
+                  text="The patented rudder-flipper steers, guides and powers the unmanned vessel
                   generating forward thrust from the pitching of the vessel in the waves of the
                   ocean. The bigger the sea state the stronger the forces. In seastates over
                   Seastate 6 the solarsail automatically lowers and the rudder passively propels the
                   vessel in the desired direction. If left in the mid position it passively turns
                   the bow of the Bluebottle into the oncoming seas giving Bluebottles unprecedented
-                  seakeeping and survival capabilities.
-                </p>
+                  seakeeping and survival capabilities."
+                  min={140}
+                  ideal={160}
+                  max={180}
+                  readMoreText="Read more"
+                />
               </Col>
               <Col xs={12} md={6} lg={6}>
                 <img src={Bluebottle} alt="Bluebottle Ocius" />
@@ -206,12 +216,18 @@ export default ({ data }) => {
                 <Heading level={3} size="medium">
                   Sensors and Communication
                 </Heading>
-                <p>
-                  Multiple sensors are available as options underwater, inside the hull and on the
+                <ReadMoreReact
+                  text="Multiple sensors are available as options underwater, inside the hull and on the
                   aft comms mast. An Integrated and networked communication system allows live
                   tracking. All vessels can be monitored while operating autonomously or controlled
-                  remotely, constantly sending data back to the control room.
-                </p>
+                  remotely, constantly sending data back to the control room."
+                  min={140}
+                  ideal={160}
+                  max={180}
+                  readMoreText="Read more"
+                />
+
+                <Spacing $value="40px" />
                 <Icon.KeelWinch
                   fill="#0fb0e1"
                   style={{
@@ -229,14 +245,18 @@ export default ({ data }) => {
                 <Heading level={3} size="medium">
                   Keel Winch Cassette
                 </Heading>
-                <p>
-                  The patented ‘keel winch’ is a cassette that displaces water ballast in the keel -
+                <ReadMoreReact
+                  text="The patented ‘keel winch’ is a cassette that displaces water ballast in the keel -
                   so adding or changing a cassette does not interfere with the displacement or trim
                   of the Bluebottle. The cassette can be flat packed and shipped to anywhere in the
                   world for customers to install, commission and test their sensor or array on the
                   winch before inserting it into the payload bay of a Bluebottle - being ready to
-                  go.
-                </p>
+                  go."
+                  min={120}
+                  ideal={180}
+                  max={200}
+                  readMoreText="Read more"
+                />
               </Col>
             </Row>
           </Segmented>
