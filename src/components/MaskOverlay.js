958b73ce335cf72cad2dd5e3009f839b380b68b6
r007 <moninsergei@gmail.com>
2020-11-25 06:09:28 +0400

    Fix mask in chrome

--- src/components/MaskOverlay.js
+++ src/components/MaskOverlay.js
@@ -3,32 +3,23 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 // Stretch the mask container at the bottom
-const MaskContainer = styled.div`
+const Curve = styled.svg`
   position: absolute;
   bottom: 0;
-  left: 0;
-  right: 0;
-  height: 80px;
-  background-color: ${(props) => props.$color || 'white'};
-  mask: url(#wave);
-  z-index: 10;
+  z-index: 1;
+
+  path {
+    fill: ${(props) => props.$color || 'white'};
+  }
 `;
 
-MaskContainer.defaultProps = {};
+Curve.defaultProps = {};
 
 const MaskOverlay = ({ color }) => {
   return (
-    <>
-      <MaskContainer $color={color} />
-      <svg viewBox="0 0 1920 80" width="0" height="0">
-        <defs>
-          <mask id="wave">
-            <rect y="2.04" width="1920" height="78.78" fill="white" />
-            <path d="M0-1438.32V46.6s502.75 77 959.59 0 960.41 0 960.41 0v-1484.92z" fill="black" />
-          </mask>
-        </defs>
-      </svg>
-    </>
+    <Curve viewBox="0 0 1920 80" $color={color}>
+      <path d="M1920.623 184.785H-.255V34.33s502.98-77 960.028 0c457.05 77 960.85 0 960.85 0z" />
+    </Curve>
   );
 };
 
