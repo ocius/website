619ee663e0385a26b97506d56c3cea520f085b92
r007 <moninsergei@gmail.com>
2020-11-26 01:22:43 +0400

    Add the "The Bluebottle’s core technology" section to usv page

--- src/pages/usv.js
+++ src/pages/usv.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Col, Row } from 'react-flexbox-grid';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import ReadMoreReact from 'read-more-react';
 import Loadable from '@loadable/component';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
@@ -224,6 +225,165 @@ export default ({ data }) => (
         </Segmented>
       </Container>
     </AccentRowWrapper>
+
+    <Container>
+      <Segmented>
+        <Row className="centered">
+          <Col xs={12} md={8} lg={8} mdOffset={2}>
+            <Spacing $value="50px" />
+            <Heading level={2} size="large" underline="center">
+              The Bluebottle’s core technology
+            </Heading>
+          </Col>
+        </Row>
+        <Spacing $value="80px" />
+        <Row>
+          <Col xs={12} md={3} lg={3}>
+            <Icon.SolarSun
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Icon.SolarFlare
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+                marginLeft: '5px',
+              }}
+            />
+            <Icon.SolarPower
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+                marginLeft: '5px',
+              }}
+            />
+            <Heading level={3} size="medium">
+              Solar Sail
+            </Heading>
+            <ReadMoreReact
+              text="When deployed, the patented hard solar sail harnesses both solar and wind energy.
+                  When not desired it automatically folds snuggly into the deck of the Bluebottle
+                  like the wing of a bird, so 100% retracted but also not taking up precious payload
+                  space within the vessel. Intelligent programming means the solar sail reacts
+                  autonomously to the sea, sun &amp; wind conditions to ensure efficient and safe
+                  operation for all mission requirements."
+              min={140}
+              ideal={160}
+              max={180}
+              readMoreText="Read more"
+            />
+
+            <Spacing $value="40px" />
+            <Icon.RudderCurrent
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Icon.RudderNav
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+                marginLeft: '5px',
+              }}
+            />
+            <Heading level={3} size="medium">
+              Rudder Flipper
+            </Heading>
+            <ReadMoreReact
+              text="The patented rudder-flipper steers, guides and powers the unmanned vessel
+                  generating forward thrust from the pitching of the vessel in the waves of the
+                  ocean. The bigger the sea state the stronger the forces. In seastates over
+                  Seastate 6 the solarsail automatically lowers and the rudder passively propels the
+                  vessel in the desired direction. If left in the mid position it passively turns
+                  the bow of the Bluebottle into the oncoming seas giving Bluebottles unprecedented
+                  seakeeping and survival capabilities."
+              min={140}
+              ideal={160}
+              max={180}
+              readMoreText="Read more"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={6}>
+            <img src={Bluebottle} alt="Bluebottle Ocius" />
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+            <Icon.SensorCommunication
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Icon.SensorLocation
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Icon.SensorTravel
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Heading level={3} size="medium">
+              Sensors and Communication
+            </Heading>
+            <ReadMoreReact
+              text="Multiple sensors are available as options underwater, inside the hull and on the
+                  aft comms mast. An Integrated and networked communication system allows live
+                  tracking. All vessels can be monitored while operating autonomously or controlled
+                  remotely, constantly sending data back to the control room."
+              min={140}
+              ideal={160}
+              max={180}
+              readMoreText="Read more"
+            />
+
+            <Spacing $value="40px" />
+            <Icon.KeelWinch
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Icon.KeelLength
+              fill="#0fb0e1"
+              style={{
+                width: '62px',
+                height: '62px',
+              }}
+            />
+            <Heading level={3} size="medium">
+              Keel Winch Cassette
+            </Heading>
+            <ReadMoreReact
+              text="The patented ‘keel winch’ is a cassette that displaces water ballast in the keel -
+                  so adding or changing a cassette does not interfere with the displacement or trim
+                  of the Bluebottle. The cassette can be flat packed and shipped to anywhere in the
+                  world for customers to install, commission and test their sensor or array on the
+                  winch before inserting it into the payload bay of a Bluebottle - being ready to
+                  go."
+              min={120}
+              ideal={180}
+              max={200}
+              readMoreText="Read more"
+            />
+          </Col>
+        </Row>
+      </Segmented>
+    </Container>
 
     <Container>
       <Row>
