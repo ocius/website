80fa89708b77e489b1ed14efa023b9d9f86ffc82
kevinwochan <kevinwochan@gmail.com>
2020-11-27 00:05:12 +1100

    updated USV page using source from website-redesign

--- src/components/MaskOverlay.js
+++ src/components/MaskOverlay.js
@@ -1,43 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { switchProp } from 'styled-tools';
 
 // Stretch the mask container at the bottom
-const MaskContainer = styled.div`
+const Curve = styled.svg`
   position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 80px;
-  background-color: ${(props) => props.$color || 'white'};
-  mask: url(#wave);
-  z-index: 10;
+  ${switchProp('$position', {
+    top: css`
+      top: 0;
+    `,
+    bottom: css`
+      bottom: 0;
+    `,
+  })}
+  z-index: 1;
+
+  path {
+    fill: ${(props) => props.$color || 'white'};
+  }
 `;
 
-MaskContainer.defaultProps = {};
+const MaskOverlay = ({ color, flipped, position }) => {
+  if (position === 'top') {
+    // Return vertically flipped version of the SVG
+    return (
+      <Curve viewBox="0 0 1920 80" $color={color} $position={position}>
+        {flipped ? (
+          <path d="M1920.623-105.892V44.563s-503.8-77-960.85 0c-457.048 77-960.028 0-960.028 0v-150.455z" />
+        ) : (
+          <path d="M-.255-105.892V44.563s503.8-77 960.85 0c457.048 77 960.028 0 960.028 0v-150.455z" />
+        )}
+      </Curve>
+    );
+  }
 
-const MaskOverlay = ({ color }) => {
   return (
-    <>
-      <MaskContainer $color={color} />
-      <svg viewBox="0 0 1920 80" width="0" height="0">
-        <defs>
-          <mask id="wave">
-            <rect y="2.04" width="1920" height="78.78" fill="white" />
-            <path d="M0-1438.32V46.6s502.75 77 959.59 0 960.41 0 960.41 0v-1484.92z" fill="black" />
-          </mask>
-        </defs>
-      </svg>
-    </>
+    <Curve viewBox="0 0 1920 80" $color={color} $position={position}>
+      {flipped ? (
+        <path d="M-.255 184.785h1920.878V34.33s-502.98-77-960.028 0c-457.05 77-960.85 0-960.85 0z" />
+      ) : (
+        <path d="M1920.623 184.785H-.255V34.33s502.98-77 960.028 0c457.05 77 960.85 0 960.85 0z" />
+      )}
+    </Curve>
   );
 };
 
 MaskOverlay.propTypes = {
   color: PropTypes.string,
+  flipped: PropTypes.bool,
+  position: PropTypes.oneOf(['top', 'bottom']),
 };
 
 MaskOverlay.defaultProps = {
-  color: '#ffffff',
+  color: 'white',
+  flipped: false,
+  position: 'bottom',
 };
 
 export default MaskOverlay;
