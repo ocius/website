b2d513bc09beaf03deed8e7621ea1c972f35d75b
r007 <moninsergei@gmail.com>
2020-11-27 06:59:47 +0400

    Fix padding on live page

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../fonts/fonts.css';
 import '../css/styles.css';
 
+/* Add padding to body to compensate navbar height */
+const PaddingWrapper = styled.div`
+  padding-top: 145px;
+`;
+
 export default ({ children }) => (
-  <>
+  <PaddingWrapper>
     <Navbar />
     {children}
     <Footer />
-  </>
+  </PaddingWrapper>
 );
