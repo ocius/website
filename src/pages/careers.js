25c2e3e4e99cca2ad0030014b6cb7480b875e595
r007 <moninsergei@gmail.com>
2020-12-04 12:51:12 +0400

    Add "Operation Supervisor" vacancy to the careers page

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -70,50 +70,146 @@ export default ({ data }) => (
 
         <Row>
           <Col xs={12} md={7} lg={7}>
-            <Heading level={3} size="medium">
-              Junior Software
-              <br />
-              and Systems Engineers
-            </Heading>
+            <Segmented multiplier={2} borderBottom>
+              <Heading level={3} size="medium">
+                Junior Software
+                <br />
+                and Systems Engineers
+              </Heading>
 
-            <p>
-              <Accent>Contact:</Accent>
-              <br />
-              lloyd@Ocius.com.au
-            </p>
+              <p>
+                <Accent>Contact:</Accent>
+                <br />
+                lloyd@Ocius.com.au
+              </p>
 
-            <p>
-              <Accent>Compensation:</Accent>
-              <br />
-              Commensurate to experience
-            </p>
+              <p>
+                <Accent>Compensation:</Accent>
+                <br />
+                Commensurate to experience
+              </p>
 
-            <p>
-              <Accent>Description:</Accent>
-              <br />
-              Are you a talented C++ Embedded Software Engineer with experience developing
-              autonomous vehicle solutions? You will be involved in a wide variety of
-              software/hardware product development activities for Ocius’ Unmanned Surface Vehicles.
-              You will contribute to product requirements and designs. You will be hands-on in the
-              development, assembly,and testing of prototype vehicles.
-            </p>
+              <p>
+                <Accent>Description:</Accent>
+                <br />
+                Are you a talented C++ Embedded Software Engineer with experience developing
+                autonomous vehicle solutions? You will be involved in a wide variety of
+                software/hardware product development activities for Ocius’ Unmanned Surface
+                Vehicles. You will contribute to product requirements and designs. You will be
+                hands-on in the development, assembly,and testing of prototype vehicles.
+              </p>
 
-            <p>
-              <Accent>
-                To be successful in obtaining this role, you will possess the following:
-              </Accent>
-            </p>
-            <ul style={{ marginTop: 0 }}>
-              <li>Extensive experience with Autonomous Vehicles.</li>
-              <li>Ability to develop on Windows, Linux and embedded systems.</li>
-              <li>C/C++/C#/JavaScript/Python development experience.</li>
-              <li>Strong mechanical aptitude.</li>
-              <li>Strong communication skills.</li>
-              <li>Self-Starter / Highly Motivated.</li>
-              <li>Commercial product development experience.</li>
-              <li>Sailing experience.</li>
-              <li>Experience with Ardupilot and supporting tools is highly desirable.</li>
-            </ul>
+              <p>
+                <Accent>
+                  To be successful in obtaining this role, you will possess the following:
+                </Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>Extensive experience with Autonomous Vehicles.</li>
+                <li>Ability to develop on Windows, Linux and embedded systems.</li>
+                <li>C/C++/C#/JavaScript/Python development experience.</li>
+                <li>Strong mechanical aptitude.</li>
+                <li>Strong communication skills.</li>
+                <li>Self-Starter / Highly Motivated.</li>
+                <li>Commercial product development experience.</li>
+                <li>Sailing experience.</li>
+                <li>Experience with Ardupilot and supporting tools is highly desirable.</li>
+              </ul>
+            </Segmented>
+
+            <Segmented>
+              <Heading level={3} size="medium">
+                Operation Supervisor
+              </Heading>
+
+              <p>
+                <Accent>Contact:</Accent>
+                <br />
+                lloyd@Ocius.com.au
+              </p>
+
+              <p>
+                <Accent>Compensation:</Accent>
+                <br />
+                Commensurate to experience
+              </p>
+
+              <p>
+                <Accent>Background:</Accent>
+                <br />
+                Ocius is an Australian company that builds autonomous Unmanned Surface Vessels
+                (USVs). These USVs use the energy of the weather – sun wind and waves – to act as
+                persistent monitoring and communications platforms. We are embarking on an ambitious
+                program of building and deploying multiple USVs for operations.
+              </p>
+              <p>
+                A key part of this program is to deploy and operate the USVs for extended periods of
+                time around Australia.
+              </p>
+
+              <p>
+                <Accent>Duties and Responsibilities:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>
+                  Planning, vessel preparation, deployment, coodination and supervision of
+                  operations.
+                </li>
+                <li>Working with the Engineering team to coordinate maintenance requirements.</li>
+                <li>Accompanying USV offshore on support boat during sea trials.</li>
+                <li>
+                  Provide local, on the ground, assessment of conditions and circumstances to aid
+                  project decisions.
+                </li>
+                <li>Coordinate and particpate in remote watchkeeping of the USVs when at sea.</li>
+                <li>
+                  Capturing and reporting lessons learnt during USV operations to assist the design
+                  and constuction of USVs.
+                </li>
+                <li>
+                  Implementing, maintaining, managing and updating all standard operating procedures
+                  and processes appropriate to the role.
+                </li>
+                <li>Maintenance of USVs</li>
+                <li>
+                  Being responsible for training other team members to the required standards,
+                  including coaching and mentoring of others.
+                </li>
+                <li>Plan mobilisation for new projects and engage with local stakeholders.</li>
+                <li>On occasion, assist with the assembly and testing of USVs.</li>
+              </ul>
+
+              <p>
+                <Accent>Requirements:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>Maritime industry or defence experience relevant to the position.</li>
+                <li>Current drivers licence.</li>
+                <li>Mechanical and/or electrical aptitude.</li>
+                <li>Strong computer literacy skills.</li>
+                <li>Ability to adhere to strict health and safety guidelines.</li>
+                <li>
+                  Excellent organisational skills and the ability to interact confidently with
+                  others.
+                </li>
+                <li>
+                  Ability to obtain and maintain a minimum of Defence Security Clearance–Baseline.
+                </li>
+                <li>
+                  Enthusiastic, positive and proactive nature with a keen attention to detail.
+                </li>
+              </ul>
+
+              <p>
+                <Accent>Desirable:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>Relevant Certificate of Competency as issued by AMSA.</li>
+                <li>Sailing experience.</li>
+                <li>Experience in maritime surveillance operations.</li>
+                <li>Keen interest in autonomous systems.</li>
+              </ul>
+            </Segmented>
           </Col>
         </Row>
       </Container>
