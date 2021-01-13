c6ec6141016e6a44281e98d81d7ab5d6dfd56d43
r007 <moninsergei@gmail.com>
2021-01-13 06:14:45 +0400

    Add "Ground Control Operator" vacancy to careers page

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -289,6 +289,100 @@ export default ({ data }) => (
                 <li>Keen interest in autonomous systems.</li>
               </ul>
             </Segmented>
+
+            <Segmented>
+              <Heading level={3} size="medium">
+                Ground Control Operator
+              </Heading>
+
+              <p>
+                <Accent>Contact:</Accent>
+                <br />
+                lloyd@Ocius.com.au
+              </p>
+
+              <p>
+                <Accent>Compensation:</Accent>
+                <br />
+                Commensurate to experience
+              </p>
+
+              <p>
+                <Accent>Background:</Accent>
+                <br />
+                Ocius is an Australian company that builds autonomous Unmanned Surface Vessels
+                (USVs). These USVs use the energy of the weather – sun wind and waves – to act as
+                persistent monitoring and communications platforms. We are embarking on an ambitious
+                program of building and deploying multiple USVs for operations.
+              </p>
+              <p>
+                A key part of this program is to deploy and operate the USVs for extended periods of
+                time around Australia. Though autonomous there is a requirement for someone on a
+                shore based ground control station to monitor the state of the vessels.
+              </p>
+
+              <p>
+                <Accent>Duties and Responsibilities:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>Safe navigation of the USV in compliance with COLREGS.</li>
+                <li>
+                  Interpretation of weather data before and during missions to ensure the safe
+                  operation of the USV.
+                </li>
+                <li>
+                  Keeping watch on cameras for other marine traffic in the area including buoys,
+                  fishing gear and any other potential hazards to the USV.
+                </li>
+                <li>Keeping watch, using external services such as marine traffic.</li>
+                <li>Maintaining mission logs including vessel systems.</li>
+                <li>Piloting of USVs from on a 24-hour shift operation schedule.</li>
+              </ul>
+
+              <p>
+                <Accent>On occasion this role may involve:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>
+                  Assisting with mobilisation of USV’s for projects including road transport to
+                  project locations. Preparation and launch of USVs at project location and on water
+                  escorting of USV to harbour limits. Demobilisation of USV post mission.
+                </li>
+                <li>Assisting with testing of USVs including launching and operating USV/RHIB.</li>
+                <li>
+                  Assiting with implementing, maintaining, managing and updating all standard
+                  operating procedures and processes appropriate to the role.
+                </li>
+              </ul>
+
+              <p>
+                <Accent>Requirements:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>
+                  Assiting with implementing, maintaining, managing and updating all standard
+                  operating procedures and processes appropriate to the role.
+                </li>
+                <li>Current drivers licence.</li>
+                <li>Ability to adhere to strict health and safety guidelines.</li>
+                <li>
+                  Ability to obtain and maintain a minimum of Defence Security Clearance–Baseline.
+                </li>
+                <li>
+                  Enthusiastic, positive and proactive nature with a keen attention to detail.
+                </li>
+              </ul>
+
+              <p>
+                <Accent>Desirable:</Accent>
+              </p>
+              <ul style={{ marginTop: 0 }}>
+                <li>Maritime industry or defence experience relevant to the position.</li>
+                <li>Relevant Certificate of Competency as issued by AMSA.</li>
+                <li>Sailing experience.</li>
+                <li>Keen interest in autonomous systems.</li>
+              </ul>
+            </Segmented>
           </Col>
         </Row>
       </Container>
