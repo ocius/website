0ed7556a841a1fb7fc33a510627159a8436bd684
r007 <moninsergei@gmail.com>
2021-01-16 07:55:13 +0400

    Move the "core technologies" section to the separate component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Loadable from '@loadable/component';
 import { Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
-import ReadMoreReact from 'read-more-react';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
 import Container from '../components/Container';
@@ -13,12 +12,10 @@ import Button from '../components/Button';
 import HeroBlock from '../components/HeroBlock';
 import Heading from '../components/Heading';
 import SolutionsBlock from '../components/SolutionsBlock';
-import Icon from '../components/Icon';
+import CoreTechnologiesBlock from '../components/CoreTechnologiesBlock';
 import { Spacing, LogoBackgroundWrapper, DarkAccentRowWrapper } from '../components/common';
 import mq from '../common/mq';
 
-// Import bluebottle illustration
-import Bluebottle from '../images/bluebottle.svg';
 import Blueybuoy from '../images/vessel-blueybuoy.svg';
 import TrimaranVessel from '../images/vessel-trimaran.svg';
 import BluebottleVessel from '../images/vessel-bluebottle.svg';
@@ -34,18 +31,6 @@ const RadarImage = styled(Img)`
 
   @media (max-width: ${mq.max[768]}) {
     margin: -10em -1em 0;
-  }
-`;
-
-const FirstColumnMobile = styled(Col)`
-  @media (max-width: ${mq.max[768]}) {
-    order: 1;
-  }
-`;
-
-const SecondColumnMobile = styled(Col)`
-  @media (max-width: ${mq.max[768]}) {
-    order: 2;
   }
 `;
 
@@ -110,166 +95,9 @@ export default ({ data }) => (
         </Row>
       </Container>
     </DarkAccentRowWrapper>
+
     <LogoBackgroundWrapper>
-      <Container>
-        <Segmented>
-          <Row className="centered">
-            <Col xs={12} md={8} lg={8} mdOffset={2}>
-              <Spacing $value="80px" />
-              <Heading level={2} size="large" weight="thick" underline="center">
-                The core technology behind Ocius
-              </Heading>
-            </Col>
-          </Row>
-          <Spacing $value="80px" $xsValue="0px" />
-          <Row>
-            <SecondColumnMobile xs={12} md={3} lg={3}>
-              <Icon.SolarSun
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Icon.SolarFlare
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                  marginLeft: '5px',
-                }}
-              />
-              <Icon.SolarPower
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                  marginLeft: '5px',
-                }}
-              />
-              <Heading level={3} size="medium" weight="thick">
-                Solar Sail
-              </Heading>
-              <ReadMoreReact
-                text="When deployed, the patented hard solar sail harnesses both solar and wind energy.
-                  When not desired it automatically folds snuggly into the deck of the Bluebottle
-                  like the wing of a bird, so 100% retracted but also not taking up precious payload
-                  space within the vessel. Intelligent programming means the solar sail reacts
-                  autonomously to the sea, sun &amp; wind conditions to ensure efficient and safe
-                  operation for all mission requirements."
-                min={140}
-                ideal={160}
-                max={180}
-                readMoreText="Read more"
-              />
-
-              <Spacing $value="40px" />
-              <Icon.RudderCurrent
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Icon.RudderNav
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                  marginLeft: '5px',
-                }}
-              />
-              <Heading level={3} size="medium" weight="thick">
-                Rudder Flipper
-              </Heading>
-              <ReadMoreReact
-                text="The patented rudder-flipper steers, guides and powers the unmanned vessel
-                  generating forward thrust from the pitching of the vessel in the waves of the
-                  ocean. The bigger the sea state the stronger the forces. In seastates over
-                  Seastate 6 the solarsail automatically lowers and the rudder passively propels the
-                  vessel in the desired direction. If left in the mid position it passively turns
-                  the bow of the Bluebottle into the oncoming seas giving Bluebottles unprecedented
-                  seakeeping and survival capabilities."
-                min={140}
-                ideal={160}
-                max={180}
-                readMoreText="Read more"
-              />
-            </SecondColumnMobile>
-            <FirstColumnMobile xs={12} md={6} lg={6}>
-              <img src={Bluebottle} alt="Bluebottle Ocius" />
-              <Spacing $value="0px" $xsValue="30px" />
-            </FirstColumnMobile>
-            <SecondColumnMobile xs={12} md={3} lg={3}>
-              <Icon.SensorCommunication
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Icon.SensorLocation
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Icon.SensorTravel
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Heading level={3} size="medium" weight="thick">
-                Sensors and Communication
-              </Heading>
-              <ReadMoreReact
-                text="Multiple sensors are available as options underwater, inside the hull and on the
-                  aft comms mast. An Integrated and networked communication system allows live
-                  tracking. All vessels can be monitored while operating autonomously or controlled
-                  remotely, constantly sending data back to the control room."
-                min={140}
-                ideal={160}
-                max={180}
-                readMoreText="Read more"
-              />
-
-              <Spacing $value="40px" $xsValue="0px" />
-              <Icon.KeelWinch
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Icon.KeelLength
-                fill="#0fb0e1"
-                style={{
-                  width: '62px',
-                  height: '62px',
-                }}
-              />
-              <Heading level={3} size="medium" weight="thick">
-                Keel Winch Cassette
-              </Heading>
-              <ReadMoreReact
-                text="The patented ‘keel winch’ is a cassette that displaces water ballast in the keel -
-                  so adding or changing a cassette does not interfere with the displacement or trim
-                  of the Bluebottle. The cassette can be flat packed and shipped to anywhere in the
-                  world for customers to install, commission and test their sensor or array on the
-                  winch before inserting it into the payload bay of a Bluebottle - being ready to
-                  go."
-                min={120}
-                ideal={180}
-                max={200}
-                readMoreText="Read more"
-              />
-            </SecondColumnMobile>
-          </Row>
-        </Segmented>
-      </Container>
+      <CoreTechnologiesBlock />
     </LogoBackgroundWrapper>
 
     <SolutionsBlock />
